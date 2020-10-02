a8b227358cb3e1d6fa24c07e06eb8ed8bf5cf601
Sandy Nguyen <69399429+sandyngu@users.noreply.github.com>
2020-10-02 17:08:40 -0400

    Presentable to Class

--- front-end/src/components/AlbumCover/AlbumCover.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function AlbumCover(props) {
-    return(
-        <img src={props.lyrics[0].albumCover} />
-    )
-}
-
-export default AlbumCover;
--- front-end/src/components/Lyrics/Lyrics.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Lyrics(props) {
-    return(
-        <h2>{props.lyrics}</h2>
-    )
-}
-
-export default Lyrics;
